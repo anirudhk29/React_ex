3e29c06fb14b0384ece640fe3fad8cabab210898
Anirudh Koolwal <koolani81@gmail.com>
2020-05-15 17:58:00 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -86,13 +87,17 @@ function RenderDish ({dish}) {
   if(dish!=null)
   {
     return(
-      <Card>
-        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-        <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform in transformProps ={{
+          exitTransform: "scale(0.5) translateY(-50%)"
+        }}>
+        <Card>
+          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+    </FadeTransform>
     );
   }
   else {
@@ -106,10 +111,15 @@ function RenderComments ({dish, postComment, dishId}) {
   {
     const rate=dish.map((item) =>{
       return(
+          <Stagger in>
             <ul id={item.id} className="list-group">
+
+                <Fade in>
               <li className="list-group-item">{item.comment}</li>
               <li className="list-group-item">--{item.author} , { new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(item.date))) }</li>
-            </ul>
+              </Fade>
+          </ul>
+        </Stagger>
       );
     });
     return(
